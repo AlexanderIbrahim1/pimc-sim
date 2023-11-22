cc0b0f9138514bf52318d39a6c22c269196c4c7b
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2023-11-21 19:57:29 -0500

    Get toml parser working

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <string>
+#include <string_view>
+
+#include <tomlplusplus/toml.hpp>
+
 
 auto main() -> int {
-    const auto message = std::string {"Hello world!"};
-    std::cout << message << '\n';
+    const auto some_toml = std::string_view {R"(
+        [library]
+        name = "toml++"
+        authors = ["Mark Gillard <mark.gillard@outlook.com.au>"]
+        cpp = 17
+    )"};
+
+    try {
+        // parse directly from a string view:
+        {
+        toml::table tbl = toml::parse(some_toml);
+        std::cout << tbl << "\n";
+        }
+
+        // parse from a string stream:
+        {
+        std::stringstream ss{ std::string{ some_toml } };
+        toml::table tbl = toml::parse(ss);
+        std::cout << tbl << "\n";
+        }
+    }
+    catch (const toml::parse_error& err) {
+        std::cerr << "Parsing failed:\n" << err << "\n";
+        return 1;
+    }
 
     return 0;
 }
