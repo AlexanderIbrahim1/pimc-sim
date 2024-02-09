d9db8a9dd191d61dabbe33ef75051cb6c8fed4af
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-02-08 21:09:29 -0500

    Make changes to MC move

--- source/pimc/centre_of_mass_move.hpp
+++ source/pimc/centre_of_mass_move.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include <concepts>
 #include <functional>
 #include <utility>
@@ -8,6 +9,7 @@
 #include <coordinates/box_sides.hpp>
 #include <coordinates/cartesian.hpp>
 #include <coordinates/measure.hpp>
+#include <environment/environment.hpp>
 #include <interactions/two_body/two_body_pointwise.hpp>
 #include <rng/distributions.hpp>
 #include <rng/generator.hpp>
@@ -37,19 +39,22 @@
 // TODO:
 // - add a move acceptance ratio logger
 
-
-namespace pimc {
+namespace pimc
+{
 
 // TODO: decide on some better names for each case
 template <std::floating_point FP, std::size_t NDIM, interact::PairDistancePotential Potential>
-class InteractionHandler {
+class InteractionHandler
+{
 public:
     explicit InteractionHandler(Potential pot, coord::BoxSides<FP, NDIM> box)
         : pot_ {std::move(pot)}
         , box_ {std::move(box)}
     {}
 
-    constexpr auto operator()(std::size_t i_particle, const worldline::Worldline<FP, NDIM>& worldline) const noexcept -> FP {
+    constexpr auto operator()(std::size_t i_particle, const worldline::Worldline<FP, NDIM>& worldline) const noexcept
+        -> FP
+    {
         auto pot_energy = FP {};
 
         const auto& points = worldline.points();
@@ -61,6 +66,8 @@ public:
             const auto distance = coord::distance_periodic(points[i_particle], points[i], box_);
             pot_energy += pot_(distance);
         }
+
+        return pot_energy;
     }
 
 private:
@@ -69,7 +76,8 @@ private:
 };
 
 template <std::floating_point FP, std::size_t NDIM>
-class CentreOfMassMovePerformer {
+class CentreOfMassMovePerformer
+{
 public:
     using Point = coord::Cartesian<FP, NDIM>;
     using Worldline = worldline::Worldline<FP, NDIM>;
@@ -78,39 +86,58 @@ public:
     CentreOfMassMovePerformer() = delete;
 
     constexpr explicit CentreOfMassMovePerformer(std::size_t n_timeslices)
-        : position_cache_(n_timeslices, Point{})
+        : position_cache_(n_timeslices, Point {})  // NOTE: initializing a vector: don't use list init here
     {}
 
     // TODO: wrap part of this in a try-catch block, to restore the original particle positions in case
     //       an exception is thrown
     constexpr void operator()(
         std::size_t i_particle,
-        Worldlines* worldlines,
-        const rng::PRNGWrapper auto& prngw,
+        Worldlines& worldlines,
+        rng::PRNGWrapper auto& prngw,
         const InteractionHandler& interact_handler,
-        const std::function<void(Point)>& step_generator
+        const std::function<void(Point)>& step_generator,
+        const envir::Environment<FP>& environment
     ) noexcept
     {
         const auto step = step_generator();
 
-        auto pot_energy_before = FP{};
-        for (const auto wline : *worldlines) {
+        // calculate energy for the current configuration
+        auto pot_energy_before = FP {};
+        for (const auto wline : worldlines) {
             pot_energy_before += interact_handler(i_particle, wline);
         }
 
         // save the current positions, and set the new ones
         for (std::size_t i_tslice {0}; i_tslice < worldlines->size(); ++i_tslice) {
-            position_cache_[i_tslice] = (*worldlines)[i_tslice][i_particle];
-            (*worldlines)[i_tslice][i_particle]
+            position_cache_[i_tslice] = worldlines[i_tslice][i_particle];
+            worldlines[i_tslice][i_particle] += step;
         }
 
-        auto pot_energy_after = FP{};
-        for (const auto wline : *worldlines) {
+        // calculate energy for the new configuration
+        auto pot_energy_after = FP {};
+        for (const auto wline : worldlines) {
             pot_energy_after += interact_handler(i_particle, wline);
         }
+
+        const auto pot_energy_diff = pot_energy_after - pot_energy_before;
+        if (pot_energy_diff >= FP {0.0}) {
+            // if the energy does not decrease, we need to look further
+            const auto boltz_factor = std::exp(-pot_energy_diff * environment.thermodynamic_tau());
+            const auto rand01 = uniform01_dist_.uniform_01(prngw);
+
+            if (boltz_factor < rand01) {
+                // restore the positions
+                for (std::size_t i_tslice {0}; i_tslice < worldlines->size(); ++i_tslice) {
+                    worldlines[i_tslice][i_particle] = position_cache_[i_tslice];
+                }
+            }
+        }
     }
+
 private:
     std::vector<Point> position_cache_ {};
+    rng::UniformFloatingPointDistribution<FP> uniform01_dist_ {};
 };
 
 }  // namespace pimc
