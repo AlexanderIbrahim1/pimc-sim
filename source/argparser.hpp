5b317be20c30128bce4b519a53cca3013c57b598
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-02-08 21:08:49 -0500

    Move argument parser to separate file, wrapped for easier use

---
+++ source/argparser.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <sstream>
+#include <stdexcept>
+#include <string_view>
+
+#include <tomlplusplus/toml.hpp>
+
+namespace argparse
+{
+
+// What do I read in?
+// - first block id
+// - last block id
+// - number of equilibrium blocks
+// - number of passes
+// - number of slices
+// - the move hyperparameters
+//   - com step size
+//   - bisection level
+
+template <typename T>
+constexpr auto cast_toml_to(const toml::table& table, std::string_view name) -> T
+{
+    const auto maybe_value = table[name].value<T>();
+    if (!maybe_value) {
+        auto err_msg = std::stringstream {};
+        err_msg << "Failed to parse '" << name << "' from the toml stream.\n";
+        throw std::runtime_error {err_msg.str()};
+    }
+
+    return *maybe_value;
+}
+
+class ArgParser
+{
+public:
+    explicit ArgParser(std::stringstream& toml_stream)
+    {
+        try {
+            const auto table = toml::parse(toml_stream);
+
+            first_block_index = cast_toml_to<std::size_t>(table, "first_block_index");
+            last_block_index = cast_toml_to<std::size_t>(table, "last_block_index");
+            n_equilibrium_blocks = cast_toml_to<std::size_t>(table, "n_equilibrium_blocks");
+            n_passes = cast_toml_to<std::size_t>(table, "n_passes");
+            n_timeslices = cast_toml_to<std::size_t>(table, "n_timeslices");
+            centre_of_mass_step_size = cast_toml_to<double>(table, "centre_of_mass_step_size");
+            bisection_level = cast_toml_to<std::size_t>(table, "bisection_level");
+            bisection_ratio = cast_toml_to<double>(table, "bisection_ratio");
+
+            parse_success_flag_ = true;
+        }
+        catch (const toml::parse_error& err) {
+            parse_success_flag_ = false;
+        }
+        catch (const std::runtime_error& err) {
+            parse_success_flag_ = false;
+        }
+    }
+
+    constexpr auto is_valid() const noexcept -> bool
+    {
+        return parse_success_flag_;
+    }
+
+    std::size_t first_block_index {};
+    std::size_t last_block_index {};
+    std::size_t n_equilibrium_blocks {};
+    std::size_t n_passes {};
+    std::size_t n_timeslices {};
+    double centre_of_mass_step_size {};
+    std::size_t bisection_level {};
+    double bisection_ratio {};
+
+private:
+    bool parse_success_flag_ {};
+};
+
+}  // namespace argparse
--- source/main.cpp
+++ source/main.cpp
@@ -5,10 +5,19 @@
 
 #include <tomlplusplus/toml.hpp>
 
+#include <argparser.hpp>
 #include <coordinates/coordinates.hpp>
 #include <interactions/two_body/two_body_pointwise.hpp>
-#include <rng/generator.hpp>
 #include <rng/distributions.hpp>
+#include <rng/generator.hpp>
+
+/*
+PLAN for main
+
+What might I read in later?
+- information needed to create the potentials
+- nearest neighbour info
+*/
 
 template <interact::PairDistancePotential Potential>
 auto takes_pointwise_pair_distance_potential(Potential pot)
@@ -18,31 +27,41 @@ auto takes_pointwise_pair_distance_potential(Potential pot)
 
 auto main() -> int
 {
-//    const auto some_toml = std::string_view {R"(
-//        [library]
-//        name = "toml++"
-//        authors = ["Mark Gillard <mark.gillard@outlook.com.au>"]
-//        cpp = 17
-//    )"};
-//
-//    try {
-//        // parse directly from a string view:
-//        {
-//            toml::table tbl = toml::parse(some_toml);
-//            std::cout << tbl << "\n";
-//        }
-//
-//        // parse from a string stream:
-//        {
-//            std::stringstream ss {std::string {some_toml}};
-//            toml::table tbl = toml::parse(ss);
-//            std::cout << tbl << "\n";
-//        }
-//    }
-//    catch (const toml::parse_error& err) {
-//        std::cerr << "Parsing failed:\n" << err << "\n";
-//        return 1;
-//    }
+    //            first_block_index = cast_toml_to<std::size_t>(table, "first_block_index");
+    //            last_block_index = cast_toml_to<std::size_t>(table, "last_block_index");
+    //            n_equilibrium_blocks = cast_toml_to<std::size_t>(table, "n_equilibrium_blocks");
+    //            n_passes = cast_toml_to<std::size_t>(table, "n_passes");
+    //            n_timeslices = cast_toml_to<std::size_t>(table, "n_timeslices");
+    //            centre_of_mass_step_size = cast_toml_to<double>(table, "centre_of_mass_step_size");
+
+    const auto toml_input = std::string_view {R"(
+        first_block_index = 0
+        last_block_index = 200
+        n_equilibrium_blocks = 20
+        n_passes = 10
+        n_timeslices = 32
+        centre_of_mass_step_size = 0.3
+        bisection_level = 3
+        bisection_ratio = 0.4
+    )"};
+
+    auto toml_stream = std::stringstream {std::string {toml_input}};
+    const auto parser = argparse::ArgParser {toml_stream};
+
+    if (!parser.is_valid()) {
+        std::cout << "PARSER DID NOT PARSE PROPERLY\n";
+    }
+    else {
+        std::cout << "PARSER WORKED!!!\n";
+        std::cout << "first_block_index = " << parser.first_block_index << '\n';
+        std::cout << "last_block_index = " << parser.last_block_index << '\n';
+        std::cout << "n_equilibrium_blocks = " << parser.n_equilibrium_blocks << '\n';
+        std::cout << "n_passes = " << parser.n_passes << '\n';
+        std::cout << "n_timeslices = " << parser.n_timeslices << '\n';
+        std::cout << "centre_of_mass_step_size = " << parser.centre_of_mass_step_size << '\n';
+        std::cout << "bisection_level = " << parser.bisection_level << '\n';
+        std::cout << "bisection_ratio = " << parser.bisection_ratio << '\n';
+    }
 
     const auto point = coord::Cartesian<double, 2> {1.0, 2.0};
     std::cout << point.as_string() << '\n';
