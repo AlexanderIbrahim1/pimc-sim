526249235655684d627426975775610ab689a3c0
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-02-26 22:08:58 -0500

    Add offsetting capabilities for the bisection level manager

--- source/pimc/bisection_level_manager.hpp
+++ source/pimc/bisection_level_manager.hpp
@@ -5,20 +5,23 @@
 #include <span>
 #include <vector>
 
+namespace pimc
+{
 
-namespace pimc {
-
-struct BisectionTriplets {
+struct BisectionTriplets
+{
     std::size_t left {};
     std::size_t mid {};
     std::size_t right {};
 
-    constexpr auto operator==(const BisectionTriplets& other) const noexcept -> bool {
+    constexpr auto operator==(const BisectionTriplets& other) const noexcept -> bool
+    {
         return (left == other.left && mid == other.mid && right == other.right);
     }
 };
 
-static constexpr auto pow_int(std::size_t base, std::size_t exponent) noexcept -> std::size_t {
+static constexpr auto pow_int(std::size_t base, std::size_t exponent) noexcept -> std::size_t
+{
     auto result = std::size_t {1};
     for (std::size_t i {0}; i < exponent; ++i) {
         result *= base;
@@ -26,18 +29,27 @@ static constexpr auto pow_int(std::size_t base, std::size_t exponent) noexcept -
     return result;
 }
 
-class BisectionLevelManager {
+static constexpr auto level_segment_size(std::size_t level) noexcept -> std::size_t
+{
+    return pow_int(2, level);
+}
+
+class BisectionLevelManager
+{
 public:
     BisectionLevelManager() = delete;
 
-    constexpr explicit BisectionLevelManager(std::size_t max_level)
+    constexpr explicit BisectionLevelManager(std::size_t max_level, std::size_t offset, std::size_t modulo)
         : max_level_ {max_level}
     {
+        ctr_check_offset_less_than_modulo_(offset, modulo);
+        ctr_check_modulo_is_positive_(modulo);
+
         const auto max_number_of_triplets = pow_int(2, max_level_) - 1;
         indices_.reserve(max_number_of_triplets);
 
         const auto max_right_endpoint = pow_int(2, max_level_);
-        indices_.emplace_back(BisectionTriplets{0, max_right_endpoint/2, max_right_endpoint});
+        indices_.emplace_back(BisectionTriplets {0, max_right_endpoint / 2, max_right_endpoint});
 
         for (std::size_t i {0}; i < max_number_of_triplets; ++i) {
             const auto current = indices_[i];
@@ -49,12 +61,21 @@ public:
             const auto left_triplet_midpoint = (current.mid + current.left) / 2;
             const auto right_triplet_midpoint = (current.mid + current.right) / 2;
 
-            indices_.emplace_back(BisectionTriplets{current.left, left_triplet_midpoint, current.mid});
-            indices_.emplace_back(BisectionTriplets{current.mid, right_triplet_midpoint, current.right});
+            indices_.emplace_back(BisectionTriplets {current.left, left_triplet_midpoint, current.mid});
+            indices_.emplace_back(BisectionTriplets {current.mid, right_triplet_midpoint, current.right});
+        }
+
+        // 2nd pass is for applying the offsets
+        for (std::size_t i {0}; i < max_number_of_triplets; ++i) {
+            auto& triplet = indices_[i];
+            triplet.left = (triplet.left + offset) % modulo;
+            triplet.mid = (triplet.mid + offset) % modulo;
+            triplet.right = (triplet.right + offset) % modulo;
         }
     }
 
-    constexpr auto triplets(std::size_t level) const noexcept -> std::span<const BisectionTriplets> {
+    constexpr auto triplets(std::size_t level) const noexcept -> std::span<const BisectionTriplets>
+    {
         const auto left_offset = static_cast<std::ptrdiff_t>(pow_int(2, level) - 1);
         const auto right_offset = static_cast<std::ptrdiff_t>(pow_int(2, level + 1) - 1);
 
@@ -67,6 +88,20 @@ public:
 private:
     std::size_t max_level_;
     std::vector<BisectionTriplets> indices_;
+
+    constexpr void ctr_check_offset_less_than_modulo_(std::size_t offset, std::size_t modulo) const
+    {
+        if (offset >= modulo) {
+            throw std::runtime_error("The offset for the bisection indices must be less than the modulo.\n");
+        }
+    }
+
+    constexpr void ctr_check_modulo_is_positive_(std::size_t modulo) const
+    {
+        if (modulo == 0) {
+            throw std::runtime_error("The modulo for the bisection indices must be positive.\n");
+        }
+    }
 };
 
-}
+}  // namespace pimc
--- test/source/bisection_level_manager_test.cpp
+++ test/source/bisection_level_manager_test.cpp
@@ -2,40 +2,55 @@
 
 #include "pimc/bisection_level_manager.hpp"
 
-
-TEST_CASE("bisection level manager") {
-    SECTION("max level 1") {
-        const auto blmanager = pimc::BisectionLevelManager {1};
+TEST_CASE("bisection level manager")
+{
+    SECTION("max level 1")
+    {
+        const auto blmanager = pimc::BisectionLevelManager {1, 0, 1000};
 
         auto it = std::begin(blmanager.triplets(0));
 
-        REQUIRE(*it == pimc::BisectionTriplets{0, 1, 2});
+        REQUIRE(*it == pimc::BisectionTriplets {0, 1, 2});
     }
 
-    SECTION("max level 2") {
-        const auto blmanager = pimc::BisectionLevelManager {2};
+    SECTION("max level 2")
+    {
+        const auto blmanager = pimc::BisectionLevelManager {2, 0, 1000};
 
         auto it0 = std::begin(blmanager.triplets(0));
         auto it1 = std::begin(blmanager.triplets(1));
 
-        REQUIRE(*it0++ == pimc::BisectionTriplets{0, 2, 4});
-        REQUIRE(*it1++ == pimc::BisectionTriplets{0, 1, 2});
-        REQUIRE(*it1++ == pimc::BisectionTriplets{2, 3, 4});
+        REQUIRE(*it0++ == pimc::BisectionTriplets {0, 2, 4});
+        REQUIRE(*it1++ == pimc::BisectionTriplets {0, 1, 2});
+        REQUIRE(*it1++ == pimc::BisectionTriplets {2, 3, 4});
     }
 
-    SECTION("max level 3") {
-        const auto blmanager = pimc::BisectionLevelManager {3};
+    SECTION("max level 3")
+    {
+        const auto blmanager = pimc::BisectionLevelManager {3, 0, 1000};
 
         auto it0 = std::begin(blmanager.triplets(0));
         auto it1 = std::begin(blmanager.triplets(1));
         auto it2 = std::begin(blmanager.triplets(2));
 
-        REQUIRE(*it0++ == pimc::BisectionTriplets{0, 4, 8});
-        REQUIRE(*it1++ == pimc::BisectionTriplets{0, 2, 4});
-        REQUIRE(*it1++ == pimc::BisectionTriplets{4, 6, 8});
-        REQUIRE(*it2++ == pimc::BisectionTriplets{0, 1, 2});
-        REQUIRE(*it2++ == pimc::BisectionTriplets{2, 3, 4});
-        REQUIRE(*it2++ == pimc::BisectionTriplets{4, 5, 6});
-        REQUIRE(*it2++ == pimc::BisectionTriplets{6, 7, 8});
+        REQUIRE(*it0++ == pimc::BisectionTriplets {0, 4, 8});
+        REQUIRE(*it1++ == pimc::BisectionTriplets {0, 2, 4});
+        REQUIRE(*it1++ == pimc::BisectionTriplets {4, 6, 8});
+        REQUIRE(*it2++ == pimc::BisectionTriplets {0, 1, 2});
+        REQUIRE(*it2++ == pimc::BisectionTriplets {2, 3, 4});
+        REQUIRE(*it2++ == pimc::BisectionTriplets {4, 5, 6});
+        REQUIRE(*it2++ == pimc::BisectionTriplets {6, 7, 8});
+    }
+
+    SECTION("max level 2, with offset and modulo")
+    {
+        const auto blmanager = pimc::BisectionLevelManager {2, 5, 8};
+
+        auto it0 = std::begin(blmanager.triplets(0));
+        auto it1 = std::begin(blmanager.triplets(1));
+
+        REQUIRE(*it0++ == pimc::BisectionTriplets {5, 7, 1});
+        REQUIRE(*it1++ == pimc::BisectionTriplets {5, 6, 7});
+        REQUIRE(*it1++ == pimc::BisectionTriplets {7, 0, 1});
     }
 }
