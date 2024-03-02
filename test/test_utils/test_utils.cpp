a74a77611abec88306fa03495dd3548b4da49cc6
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-03-01 22:59:56 -0500

    Improve the tests

--- test/test_utils/test_utils.cpp
+++ test/test_utils/test_utils.cpp
@@ -1,8 +1,9 @@
 #include <filesystem>
 #include <sstream>
 #include <stdexcept>
+#include <string>
 
-#include "filesearch.hpp"
+#include "test_utils.hpp"
 
 namespace test_utils
 {
@@ -23,6 +24,24 @@ auto resolve_project_path(const std::filesystem::path& rel_filepath) -> std::fil
     auto err_msg = std::stringstream {};
     err_msg << "Error: relative path not resolved: '" << rel_filepath.string() << "'\n";
     throw std::runtime_error(err_msg.str());
+}
+
+void skip_lines_starting_with(std::istream& stream, char c)
+{
+    auto line = std::string {};
+    while (std::getline(stream, line)) {
+        if (line.empty()) {
+            continue;
+        }
+
+        if (line[0] != c) {
+            stream.putback('\n');
+            for (std::size_t i {line.size()}; i > 0; --i) {
+                stream.putback(line[i - 1]);
+            }
+            break;
+        }
+    }
 }
 
 }  // namespace test_utils
