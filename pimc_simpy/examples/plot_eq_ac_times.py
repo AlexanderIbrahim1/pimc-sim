743f8d29e1d8e0e2ae76432de71722eaa0c4b0e9
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-09-03 10:48:51 -0400

    Find equilbration and autocorrelation times for P=64 simulations

--- pimc_simpy/examples/plot_eq_ac_times.py
+++ pimc_simpy/examples/plot_eq_ac_times.py
@@ -3,14 +3,18 @@ This script plots certain estimated properties collected from the Monte Carlo si
 to find the equilbration and autocorrelation times.
 """
 
+import sys
 from pathlib import Path
 
 from project_info import ProjectInfo
 
 from pimc_simpy.data import read_property_data
+from pimc_simpy.data import PropertyData
+from pimc_simpy.data import between_epochs
 from pimc_simpy.manage import get_abs_job_output_dirpath
 from pimc_simpy.manage import SimulationProjectInfo
 from pimc_simpy.plotting import plot_property_rescaled
+from pimc_simpy.statistics import autocorrelation_time_from_data
 
 
 class ProjectInfo(SimulationProjectInfo):
@@ -39,7 +43,18 @@ POTENTIAL_ENERGY_FILENAME: str = "pair_potential.dat"
 KINETIC_ENERGY_FILENAME: str = "kinetic.dat"
 
 
-def plot_energies(sim_id: int) -> None:
+def parse_sim_id(argv: list[str]) -> int:
+    if len(argv) != 2:
+        raise RuntimeError("python plot_eq_ac_times.py <sim_id>")
+
+    sim_id = int(argv[1])
+    if sim_id < 0 or sim_id > 30:
+        raise ValueError("sim_id must be in [0, 30], inclusive end")
+
+    return sim_id
+
+
+def get_potential_and_kinetic_data(sim_id: int) -> tuple[PropertyData, PropertyData]:
     info = ProjectInfo()
 
     potential_filepath = get_abs_job_output_dirpath(info, sim_id) / POTENTIAL_ENERGY_FILENAME
@@ -48,9 +63,30 @@ def plot_energies(sim_id: int) -> None:
     potential_data = read_property_data(potential_filepath)
     kinetic_data = read_property_data(kinetic_filepath)
 
+    return potential_data, kinetic_data
+
+
+def plot_energies(sim_id: int) -> None:
+    potential_data, kinetic_data = get_potential_and_kinetic_data(sim_id)
     plot_property_rescaled([potential_data, kinetic_data], labels=["potential", "kinetic"])
 
 
+def calculate_autocorrelation(sim_id: int, n_equilibration_sweeps: int) -> None:
+    potential_data, kinetic_data = get_potential_and_kinetic_data(sim_id)
+
+    potential_data = between_epochs(n_equilibration_sweeps, potential_data.values.size, potential_data)
+    kinetic_data = between_epochs(n_equilibration_sweeps, kinetic_data.values.size, kinetic_data)
+
+    potential_auto_time = autocorrelation_time_from_data(potential_data.values)
+    kinetic_auto_time = autocorrelation_time_from_data(kinetic_data.values)
+
+    print(f"potential_auto_time = {potential_auto_time}")
+    print(f"kinetic_auto_time = {kinetic_auto_time}")
+
+
 if __name__ == "__main__":
-    sim_id = 30
-    plot_energies(sim_id)
+    # sim_id = parse_sim_id(sys.argv)
+    # plot_energies(sim_id)
+    for sim_id in range(31):
+        print(f"sim_id = {sim_id}")
+        calculate_autocorrelation(sim_id, 500)
