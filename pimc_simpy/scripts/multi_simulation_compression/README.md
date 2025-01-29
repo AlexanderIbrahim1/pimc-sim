4e54f24bde3b471962495c568727d00766514df7
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-01-29 00:13:54 -0500

    Begin the compression of the parallel simulations data

---
+++ pimc_simpy/scripts/multi_simulation_compression/README.md
@@ -0,0 +1,7 @@
+For the N = 180, P = 960 simulations, I ran 1000 separate simulations, each performing a single block
+  - about 980 of those simulations were completed
+
+This is an annoying format to work with
+  - I need to combine the data from all the 1000 separate simulations into the same format as
+    if all the blocks were performed in a single simulation
+  - this will make it easier for the existing code to analyze the data
---
+++ pimc_simpy/scripts/multi_simulation_compression/block_index_mapping.py
@@ -0,0 +1,70 @@
+"""
+Only about 980 of the 1000 jobs finished. The analysis scripts have trouble working
+with block indices when certain ones are missing. To make it easier for the analysis
+scripts, the job indices (between 000 and 999) will be mapped to artificial block
+indices (between 000 and however many jobs completed).
+"""
+
+from pathlib import Path
+from io import TextIOWrapper
+
+BLOCK_INDEX_FILEPATH: Path = Path(".", "block_to_job_index_map.txt")
+
+
+def read_block_to_job_index_map() -> dict[int, int]:
+    def read_job_and_block_index(line: str) -> tuple[int, int]:
+        tokens = line.split()
+        i_block = int(tokens[0])
+        i_job = int(tokens[1])
+
+        return i_block, i_job
+
+    jobs_to_blocks: dict[int, int] = {}
+    with open(BLOCK_INDEX_FILEPATH, "r") as instream:
+        for line in instream:
+            i_block, i_job = read_job_and_block_index(line)
+            jobs_to_blocks[i_block] = i_job
+
+    return jobs_to_blocks
+
+
+def _get_abs_jobs_dirpath() -> Path:
+    abs_local_repo_dirpath = Path("/home/a68ibrah/research/simulations/pimc-sim")
+    rel_jobs_dirpath = Path("pimc_simpy/playground/twothreefour_body/pert2b3b4b/n180_pert2b3b4b_round1/simulations")
+
+    return abs_local_repo_dirpath / rel_jobs_dirpath
+
+
+def _get_abs_indicator_filepath(i_job: int, abs_jobs_dirpath: Path) -> Path:
+    rel_output_dirpath = Path(f"job_{i_job:0>3d}", "output")
+    indicator_filename = "quadruplet_potential.dat"
+
+    return abs_jobs_dirpath / rel_output_dirpath / indicator_filename
+
+
+def _write_block_index_map(outstream: TextIOWrapper, finished_jobs: list[int]) -> None:
+    header = "# [job index]   [block index]"
+    for i_block, i_job in enumerate(finished_jobs):
+        line = f"{i_block:0>3d}   {i_job:0>3d}\n"
+        outstream.write(line)
+
+
+def main() -> None:
+    abs_jobs_dirpath = _get_abs_jobs_dirpath()
+
+    n_jobs = 1000
+    finished_jobs: list[int] = []
+
+    for i in range(n_jobs):
+        abs_indicator_filepath = _get_abs_indicator_filepath(i, abs_jobs_dirpath)
+        if abs_indicator_filepath.exists():
+            finished_jobs.append(i)
+
+    with open(BLOCK_INDEX_FILEPATH, "w") as fout:
+        _write_block_index_map(fout, finished_jobs)
+
+
+if __name__ == "__main__":
+    main()
+    jobs_to_blocks = read_block_to_job_index_map()
+    print(jobs_to_blocks)
